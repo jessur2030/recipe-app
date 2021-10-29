99e37d2bd3c769a61ddcee6d1b463360f0bd98dd
jessur2030 <jessur2030@gmail.com>
2021-10-29 13:28:20 -0400

    fracty bug fixes

--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -1,8 +1,8 @@
 //import icons from '../img/icons.svg'; // Parcel 1
+import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 // import { Fraction } from 'fractional';
-import { fracty } from 'fracty';
-import View from './View.js';
+import fracty from 'fracty';
 
 //@@@ Class RecipeView{}
 class RecipeView extends View {
