521142d932912cc64114218de4507c2471af25ee
jessur2030 <jessur2030@gmail.com>
2021-10-29 11:59:37 -0400

    added fracty

--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -1,6 +1,7 @@
 //import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+import { fracty } from 'fracty';
 import View from './View.js';
 
 //@@@ Class RecipeView{}
@@ -138,7 +139,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+      ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ingredient.unit}</span>
