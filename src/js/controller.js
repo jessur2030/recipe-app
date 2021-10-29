f2ca72f77edafbc370255312ccddfc627d84bf4b
jessur2030 <jessur2030@gmail.com>
2021-10-29 14:55:14 -0400

    del test func

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,10 +128,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const testFun = function () {
-  console.log(`This is a test`);
-};
-
 //subscriber : init
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -141,7 +137,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  testFun();
 };
 
 init();
